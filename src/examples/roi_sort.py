b71f2cb5e045c735a5f896bc4de08c174ea43fb9
Lukas <llo@bluewin.ch>
2020-01-17 15:37:51 +0100

    Implemented in the roi_sort.py example the sorting of rois fitting to a rectangular grid.

--- src/examples/roi_sort.py
+++ src/examples/roi_sort.py
@@ -25,6 +25,9 @@ ROI_IMAGE = INPUT_DATA_DIR + "irbacklit_20200109_1525_4x4x6_squaremask_valued.pn
 
 logfile = OUTPUT_DATA_DIR + 'roi_sort.log'
 
+dbgImgWinSizeX = 1600
+dbgImgWinSizeY = 1200
+
 # setup logging
 logging.basicConfig(filename=logfile, level=logging.INFO)
 
@@ -48,7 +51,6 @@ logging.info("building rois")
 rois = roi_builder.build(None)  # use image already loaded by ImgMaskROIBuilder instance
 
 image = np.zeros_like(roi_builder._mask);
-#image = np.full_like(roi_builder._mask, 255);
 
 
 #print("Rois: ");
@@ -68,22 +70,24 @@ for r in rois:
 
 print("Found %d rois" % len(rois))
 # show the image
-cv2.namedWindow("ROIMask Centers", cv2.WINDOW_NORMAL)
-cv2.resizeWindow("ROIMask Centers", 800, 600)
-cv2.imshow("ROIMask Centers", image)
-cv2.waitKey(0)
+#cv2.namedWindow("ROIMask Centers", cv2.WINDOW_NORMAL)
+#cv2.resizeWindow("ROIMask Centers", dbgImgWinSizeX, dbgImgWinSizeY)
+#cv2.imshow("ROIMask Centers", image)
+#cv2.waitKey(0)
 
 class RoiSpace():
   def __init__(self, roi):
     """
     Class representing a region of interest(ROI) augmented by it's
-    center of gravity (center point):
+    center of gravity (center point) and a two dimensional category numbering.
     """
     self._roi = roi;
     # compute the center of the contour
     M = cv2.moments(roi.polygon)
     self._cX = int(M["m10"] / M["m00"])
     self._cY = int(M["m01"] / M["m00"])
+    self._xCategory = 0
+    self._yCategory = 0
     
     
 class RoiCenters():
@@ -95,17 +99,59 @@ class RoiCenters():
     """
     self._rois = rois
     self._roiSpaces = []
-    self._localMaximaX = []
     for r in self._rois:
       self._roiSpaces.append(RoiSpace(r))
+    self._xSorted = sorted(self._roiSpaces, key=lambda a: a._cX)
+    self._ySorted = sorted(self._roiSpaces, key=lambda a: a._cY)
+    self._xCategoryCnt = 0
+    self._yCategoryCnt = 0
       
 
   def printRoiCenters(self):
     for r in self._roiSpaces:
       print("Roi %d: (%d,%d)" % (r._roi.idx, r._cX, r._cY))
   
+  def gridSort(self, xTolerance, yTolerance):
+    """
+    Tries to arrange the Rois into a rectangular grid and assign values to them
+    to be able to sort them into the grid coordinates from left to right and top to down
+    """
+    _xCategory = -1
+    _prevX = 0
+    for r in self._xSorted:
+      _xDistance = r._cX - _prevX
+      if (_xDistance > xTolerance):
+        _xCategory += 1
+      r._xCategory = _xCategory
+      _prevX = r._cX
+    self._xCategoryCnt = _xCategory + 1
+
+    _yCategory = -1
+    _prevY = 0
+    for r in self._ySorted:
+      _yDistance = r._cY - _prevY
+      if (_yDistance > yTolerance):
+        _yCategory += 1
+      r._yCategory = _yCategory
+      _prevY = r._cY
+    self._yCategoryCnt = _yCategory + 1
+
+    for rs in self._roiSpaces:
+      rs._roi._value = rs._yCategory * self._xCategoryCnt + rs._xCategory
+#      print("Roi %d: (%d,%d), category: (%d,%d)" % (rs._roi.idx, rs._cX, rs._cY, rs._xCategory, rs._yCategory))
+
+    _valueSorted =  sorted(self._roiSpaces, key=lambda a: a._roi._value)
+
+    for vs in _valueSorted:
+      print("Roi %d: (%d,%d), value: %d, category: (%d,%d)" % 
+            (vs._roi.idx, vs._cX, vs._cY, vs._roi._value + 1, vs._xCategory, vs._yCategory))
+
+    print("Categories X: %d, Y: %d" % (self._xCategoryCnt, self._yCategoryCnt))
+
+
 
 roiCenters = RoiCenters(rois)
-roiCenters.printRoiCenters()
+#roiCenters.printRoiCenters()
+roiCenters.gridSort(50, 50)
 
 
